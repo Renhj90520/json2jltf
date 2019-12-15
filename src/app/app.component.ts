9071773c9359ca5709688d6bf492303a045341f2
RenHaojie <renhaojie520@live.cn>
2019-12-16 07:55:19 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { LegacyJSONLoader } from 'three/examples/jsm/loaders/deprecated/LegacyJSONLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'json2gltf';
+export class AppComponent implements OnInit {
+  customRequest;
+  nzAccept = '.json';
+
+  ngOnInit(): void {
+    this.customRequest = item => {
+      const fileName = item.file.name;
+      const extension = fileName.split('.').pop();
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const data = e.target.result;
+        const jsonLoaser = new LegacyJSONLoader();
+        const { geometry, materials } = jsonLoaser.parse(
+          JSON.parse(data),
+          THREE.LoaderUtils.extractUrlBase('/')
+        );
+      };
+      reader.readAsText(item.file);
+      return of(1).subscribe();
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NZ_I18N, zh_CN, NzUploadModule } from "ng-zorro-antd";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { registerLocaleData } from "@angular/common";
+import zh from "@angular/common/locales/zh";
+
+registerLocaleData(zh);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzUploadModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
